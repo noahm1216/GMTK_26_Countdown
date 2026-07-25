4955191562fd0f7188f378f328a98f2117bd58c7
Noah <16135831+noahm1216@users.noreply.github.com>
2026-07-25 11:59:35 -0700

    new modifier l divide halved

--- CountdownGMTK26/Assets/Scripts/Managers/ModifierManager.cs
+++ CountdownGMTK26/Assets/Scripts/Managers/ModifierManager.cs
@@ -31,26 +31,21 @@ public class ModifierManager : MonoBehaviour
 
     public CardModifierBase ReturnModiferFromAllModifiers(EffectAlignment _alignment, int _id = -1, bool _repeatOkay = true) // set to false if we dont want repeat mod/runes
     {
-        print("Return Modifier From All Modifier List");
 
         CardModifierBase cardmodToReturn = null;
         if (_id > -1 && _id <= allModifiers.Count) // grab a specific card
         {
-            print($"Grabbing Mod By ID {_id}");
             if (!ownedModifiers.Contains(allModifiers[_id]) || ownedModifiers.Contains(allModifiers[_id]) && CardModPlayable(allModifiers[_id]) && _repeatOkay)
                 cardmodToReturn = allModifiers[_id];
         }
 
         if (cardmodToReturn == null)
         {
-            print($"NO Mod grabbed By ID {_id}");
-
             List<CardModifierBase> possibleCardmods = new List<CardModifierBase>();
             for (int i = 0; i < allModifiers.Count; i++)
             {
                 if (allModifiers[i].alignment == _alignment)
-                {
-                    print($"Found modifier of similar alignment {allModifiers[i].modName}");
+                {                    
                     if (!ownedModifiers.Contains(allModifiers[i]) || ownedModifiers.Contains(allModifiers[i]) && CardModPlayable(allModifiers[i]) && _repeatOkay)
                     { possibleCardmods.Add(allModifiers[i]); print($"Add possible mod: {allModifiers[i].modName}"); }
                 }
