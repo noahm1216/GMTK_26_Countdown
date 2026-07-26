d95ba7b50beb617fefb2b0c333d5ec59f68a8a28
Brett <babsportsfan@gmail.com>
2026-07-25 23:33:15 -0400

    Added two new modifers

---
+++ CountdownGMTK26/Assets/Scripts/CardModifiers/CMod_MultiplyAll.cs
@@ -0,0 +1,18 @@
+
+
+public class CMod_MultiplyAll: CardModifierBase
+{
+    public override float OnCardSelected(CardData _cardData, float _timeCur = -1)
+    {
+        if (timesUsableInGame < timesApplied || timesUsableInGame == -1) // checks if card is used too many times
+        {
+            if (_cardData) // checks if there was data sent 
+            {
+                // if condition is met
+                timesApplied++; return _cardData.value * 2f; // return value
+
+            }
+        }
+        return base.OnCardSelected(_cardData, _timeCur); // otherwise call base value
+    }
+}
