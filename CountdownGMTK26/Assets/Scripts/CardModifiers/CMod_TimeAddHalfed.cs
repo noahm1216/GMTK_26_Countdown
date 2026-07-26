5b008700c05df0394f65b968b41b5dffa628f94a
Brett <babsportsfan@gmail.com>
2026-07-26 00:58:49 -0400

    New Modifier

---
+++ CountdownGMTK26/Assets/Scripts/CardModifiers/CMod_TimeAddHalfed.cs
@@ -0,0 +1,18 @@
+
+
+public class CMod_TimeAddHalfed : CardModifierBase
+{
+    public override float OnCardSelected(CardData _cardData, float _timeCur = -1)
+    {
+        if (timesUsableInGame < timesApplied || timesUsableInGame == -1) // checks if card is used too many times
+        {
+            if (_cardData) // checks if there was data sent 
+            {
+                if (_cardData.effect == CardEffectType.AddTime) // if condition is met
+                { timesApplied++; return _cardData.value * .5f; } // return value
+
+            }
+        }
+        return base.OnCardSelected(_cardData, _timeCur); // otherwise call base value
+    }
+}
